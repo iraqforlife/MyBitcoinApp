cfc5ece5e9f74f4cf09dd926de3293bfc8d385c5
iraqforlife <ahmad.al-taher.1@etsmtl.net>
2025-03-31 11:19:37 -0400

    Refactor user list component to use Angular Material table; improve error handling and loading states

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -3,10 +3,13 @@ import { UsersService } from '../../services/users.service';
 import { User } from '../../dto/user.dto';
 import { setErrorMessage } from '../../utils/error-message';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './user-list.component.html',
   styles: ``,
 })
@@ -20,4 +23,6 @@ export class UserListComponent {
   error = computed(() => this.UsersResource.error() as HttpErrorResponse);
   errorMessage = computed(() => setErrorMessage(this.error(), 'vehicle'));
   isLoading = computed(() => this.UsersResource.isLoading());
+
+  displayedColumns: string[] = ['email', 'firstName', 'lastName'];
 }
